328f8651ccc0cee22228ab7b4b9515eec6f547b2
Yuki Yamamura <yuki.yamamura@icloud.com>
2022-12-14 05:08:02 +0900

    chore: add some colors and fonts for Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ['./*.html'],
   theme: {
     extend: {},
+    colors: {
+      darkCyan: 'hsl(158, 36%, 37%)',
+      cream: 'hsl(30, 38%, 92%)',
+      veryDarkCyan: 'hsl(158, 36%, 17%)',
+      darkGrayishBlue: 'hsl(228, 12%, 48%)',
+      white: 'hsl(0, 0%, 100%)',
+      black: 'hsl(0, 0%, 0%)',
+    },
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+      fraunces: ['Fraunces', 'sans-serif'],
+    },
+    letterSpacing: {
+      widest: '.15em',
+    },
+    maxWidth: {
+      sm: '375px',
+      xl: '576px',
+    },
   },
   plugins: [],
 };
